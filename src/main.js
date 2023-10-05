2cf1fd0897f0f86c6e90239f8d7b46a737e4fcb7
marve10s <igrimanigroman@gmail.com>
2023-10-05 23:26:30 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import '../styles/modern-normal.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/welcome.css'
+import '../styles/components/about.css'
+import '../styles/components/featured.css'
+import '../styles/components/work.css'
+import '../styles/components/contact.css'
+import '../styles/components/footer.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utility.css'
+
+import mobileNav from './utils/mobile-mav'
+import darkMode from './utils/dark-mode'
+
+
+mobileNav();
+darkMode();
